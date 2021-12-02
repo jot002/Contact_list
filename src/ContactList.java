03eb7f754d6850a759b2e3ab444df62261693f5e
jot002 <jot002@ucsd.edu>
2021-12-02 11:10:12 -0800

    changes

--- src/ContactList.java
+++ src/ContactList.java
@@ -6,10 +6,10 @@ public class ContactList {
     private ArrayList<Person> contactList;
     private int totalNum;
 
-//    public ContactList() {
-//        this.contactList = new ArrayList<Person>();
-//        this.totalNum = 0;
-//    }
+    public ContactList() {
+        this.contactList = new ArrayList<Person>();
+        this.totalNum = 0;
+    }
 
     public boolean createContact(Person person) {
         for (Person p : this.contactList) {
@@ -22,82 +22,82 @@ public class ContactList {
         return true;
     }
 
-    public boolean lookupContact(String name) {
-        for (Person p : this.contactList) {
-            if (p.getName().equals(name)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public Person getContact(String name) {
-        for (Person p : this.contactList) {
-            if (p.getName().equals(name)) {
-                return p;
-            }
-        }
-        return null;
-    }
-
-    public Person[] getContactByRange(String start, String end) {
-        if (start.compareTo(end) > 0) {
-            throw new IllegalArgumentException();
-        }
-        ArrayList<Person> rangedContacts = new ArrayList<Person>();
-        for (Person p : this.contactList) {
-            if ((start.compareTo(p.getName()) <= 0) && (end.compareTo(p.getName()) > 0)) {
-                rangedContacts.add(p);
-            }
-        }
-        return rangedContacts.toArray(new Person[rangedContacts.size()]);
-//        Person[] reducedContacts = new Person[rangedContacts.size()];
-//        int i = 0;
-//        for (Person p : rangedContacts) {
-//            reducedContacts[i] = p;
-//            i++;
+//    public boolean lookupContact(String name) {
+//        for (Person p : this.contactList) {
+//            if (p.getName().equals(name)) {
+//                return true;
+//            }
 //        }
-//        return reducedContacts;
-    }
+//        return false;
+//    }
+//
+//    public Person getContact(String name) {
+//        for (Person p : this.contactList) {
+//            if (p.getName().equals(name)) {
+//                return p;
+//            }
+//        }
+//        return null;
+//    }
 
-    public boolean deleteContact(String name) {
-        for (Person p : this.contactList) {
-            if (p.getName().equals(name)) {
-                this.contactList.remove(p);
-                this.totalNum--;
-                return true;
-            }
-        }
-        return false;
-    }
+//    public Person[] getContactByRange(String start, String end) {
+//        if (start.compareTo(end) > 0) {
+//            throw new IllegalArgumentException();
+//        }
+//        ArrayList<Person> rangedContacts = new ArrayList<Person>();
+//        for (Person p : this.contactList) {
+//            if ((start.compareTo(p.getName()) <= 0) && (end.compareTo(p.getName()) > 0)) {
+//                rangedContacts.add(p);
+//            }
+//        }
+//        return rangedContacts.toArray(new Person[rangedContacts.size()]);
+////        Person[] reducedContacts = new Person[rangedContacts.size()];
+////        int i = 0;
+////        for (Person p : rangedContacts) {
+////            reducedContacts[i] = p;
+////            i++;
+////        }
+////        return reducedContacts;
+//    }
 
-    public int size() {
-        return this.totalNum;
-    }
+//    public boolean deleteContact(String name) {
+//        for (Person p : this.contactList) {
+//            if (p.getName().equals(name)) {
+//                this.contactList.remove(p);
+//                this.totalNum--;
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//
+//    public int size() {
+//        return this.totalNum;
+//    }
 
-    public String[] fetchAllNames() {
-        ArrayList<String> allNames = new ArrayList<String>();
-        for (Person p : this.contactList) {
-            allNames.add(p.getName());
-        }
-        Collections.sort(allNames);
-        return allNames.toArray(new String[allNames.size()]);
-    }
-
-    public String[] fetchAllPhoneNumbers() {
-        ArrayList<String> allNums = new ArrayList<String>();
-        for (Person p : this.contactList) {
-            for (String num : p.getPhoneNumbers()) {
-                allNums.add(num);
-            }
-        }
-        Collections.sort(allNums);
-        ArrayList<String> temp = new ArrayList<String>();
-        for (String num : allNums) {
-            if (!allNums.contains(num)) {
-                allNums.add(num);
-            }
-        }
-        return allNums.toArray(new String[allNums.size()]);
-    }
+//    public String[] fetchAllNames() {
+//        ArrayList<String> allNames = new ArrayList<String>();
+//        for (Person p : this.contactList) {
+//            allNames.add(p.getName());
+//        }
+//        Collections.sort(allNames);
+//        return allNames.toArray(new String[allNames.size()]);
+//    }
+//
+//    public String[] fetchAllPhoneNumbers() {
+//        ArrayList<String> allNums = new ArrayList<String>();
+//        for (Person p : this.contactList) {
+//            for (String num : p.getPhoneNumbers()) {
+//                allNums.add(num);
+//            }
+//        }
+//        Collections.sort(allNums);
+//        ArrayList<String> temp = new ArrayList<String>();
+//        for (String num : allNums) {
+//            if (!allNums.contains(num)) {
+//                allNums.add(num);
+//            }
+//        }
+//        return allNums.toArray(new String[allNums.size()]);
+//    }
 }
