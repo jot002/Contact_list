45310328dc0bf37c45ea403977c15fba3d7b4d74
jot002 <jot002@ucsd.edu>
2021-12-02 13:42:44 -0800

    changes

--- src/ContactList.java
+++ src/ContactList.java
@@ -78,18 +78,19 @@ public class ContactList {
     }
 
     public String[] fetchAllPhoneNumbers() {
-        ArrayList<String> allNums = new ArrayList<String>();
-        for (Person p : this.contactList) {
-            allNums.addAll(p.getPhoneNumbers());
-        }
-        Collections.sort(allNums);
-        ArrayList<String> temp = new ArrayList<String>();
-        for (String num : allNums) {
-            if (!temp.contains(num)) {
-                temp.add(num);
-            }
-        }
-        return temp.toArray(new String[temp.size()]);
+//        ArrayList<String> allNums = new ArrayList<String>();
+//        for (Person p : this.contactList) {
+//            allNums.addAll(p.getPhoneNumbers());
+//        }
+//        Collections.sort(allNums);
+//        ArrayList<String> temp = new ArrayList<String>();
+//        for (String num : allNums) {
+//            if (!temp.contains(num)) {
+//                temp.add(num);
+//            }
+//        }
+//        return temp.toArray(new String[temp.size()]);
+        return null;
     }
 }
 
